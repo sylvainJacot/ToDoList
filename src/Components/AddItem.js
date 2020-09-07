5931ceaf71a7f5ebb817107eabb3e24f679125ea
coatriver <sylvain.jacot@hotmail.be>
2020-09-07 20:33:44 +0200

    Input created and first styling

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -1,10 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
+export const Form = styled.form`
+
+`;
+
+export const Input = styled.input`
+border-radius: 5px;
+padding: 8px;
+width: 500px;
+border: none;
+transition: .3s;
+font-size: 1rem;
+font-weight: 600;
+outline: none;
+
+`;
 
 const AddItem = () => {
     return <>
-        AddItem
+        <Form>
+            <Input
+            type={"text"}
+            ></Input>
+        </Form>
     </>
 
 };
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -7,6 +7,8 @@ export const Wrapper = styled.div`
 width: 100%;
 height: 100vh;
 background-color: ${Colors.DarkGrey};
+display: flex;
+justify-content: center;
 `;
 
 const ToDoList = () => {
