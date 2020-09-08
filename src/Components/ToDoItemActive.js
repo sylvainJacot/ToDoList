57156ec146398b8438773e557d981d89ae6d7385
coatriver <sylvain.jacot@hotmail.be>
2020-09-08 17:57:44 +0200

    Cleaner code for AddItem completed and active

--- src/Components/ToDoItemActive.js
+++ src/Components/ToDoItemActive.js
@@ -1,77 +1,17 @@
 import React from "react";
-import styled from "styled-components";
-import {Colors} from "./Colors";
+import ToDoItem from "./ToDoItem";
 
-export const Wrapper = styled.div`
-position: relative;
-width: 100%;
-display: flex;
-justify-content: space-between;
-align-items: center;
-transition: .2s;
-padding: 12px 0px;
-&:hover {
-transform: scale(1.01);
-transition: .2s;
-cursor: pointer;
-}
-`;
 
-export const RightSide = styled.div`
-display: flex;
-align-items: center;
-
-& > span {
-display: inline-block;
-margin-left: 8px;
-opacity: .3;
-&:hover {
-opacity: 1;
-}
-}
-
-`;
-
-export const Task = styled.span`
-position: relative;
-display: block;
-font-size: 1rem;
-font-weight: 700;
-color: ${props => props.isCompleted? Colors.MediumGrey : Colors.LightGrey};
-transition: .1s;
-&::after {
-display: block;
-position: absolute;
-content: "";
-background-color: ${props => props.isCompleted? Colors.MediumGrey : Colors.LightGrey};
-right: ${props => props.isCompleted?"0":"100%"};
-left: 0;
-bottom:50%;
-height: 2px;
-transform: translate(0,50%);
-transition: .1s;
-}
-
-`;
-
-export const Time = styled.div`
-color: ${Colors.MediumGrey};
-font-weight: 400;
-display: inline-block;
-`;
 
 const ToDoItemActive = ({todo, handleItemClick, index, handleRemove}) => {
     if (!todo.isCompleted) {
     return (
-        <Wrapper>
-            <Task
-                isCompleted={todo.isCompleted}
-                onClick={() => handleItemClick(index)}>{todo.message}</Task>
-            <RightSide>
-                <Time>{todo.creationTime}</Time>
-                <span onClick={() => handleRemove(index)}>❌</span>
-            </RightSide>
-        </Wrapper>
+        <ToDoItem
+            todo={todo}
+            handleItemClic={handleItemClick}
+            index={index}
+            handleRemove={handleRemove}
+        />
     )
     } else {
         return null;
